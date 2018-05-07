f65182159d083f85a9b04e77a98428c61b4d0c3b
lcs-tdauletov <32198299+lcs-tdauletov@users.noreply.github.com>
2018-05-07 12:37:18 -0400

    Done

--- RSANumbers/main.swift
+++ RSANumbers/main.swift
@@ -8,11 +8,65 @@
 
 import Foundation
 
-// INPUT
-// Collect and filter user input here
+var providedLowerLim = 0
+var printedLowerLim = 0
+var providedUpperLim = 0
+var printedUpperLim = 0
 
-// PROCESS
-// Implement the primary logic of the problem here
+while true {
+    
+    print("Give me some lower limit")
+    
+    guard let givenInputL = readLine() else {
+        continue
+    }
+    guard let integerInputL = Int(givenInputL) else {
+        continue
+    }
+    if integerInputL < 1 || integerInputL > 1_000 {
+        continue
+    }
+    providedLowerLim = integerInputL
+    printedLowerLim = integerInputL
+    
+    break
+}
 
-// OUTPUT
-// Report results to the user here
+while true {
+    
+    print("Give me some upper limit")
+    
+    guard let givenInputU = readLine() else {
+        continue
+    }
+    guard let integerInputU = Int(givenInputU) else {
+        continue
+    }
+    if integerInputU < providedLowerLim {
+        continue
+    }
+    providedUpperLim = integerInputU
+    
+    break
+}
+
+var arrayForRSA : [Int] = []
+
+
+while providedLowerLim < providedUpperLim + 1 {
+    var countThediv = 0
+    inner: for divisor in 1...providedLowerLim {
+        if providedLowerLim % divisor == 0 {
+            countThediv += 1
+        }
+    }
+    if countThediv == 4 {
+        arrayForRSA.append(providedLowerLim)
+    }
+    
+    providedLowerLim += 1
+}
+
+print("There number of RSA numbers between \(printedLowerLim) and \(providedUpperLim) is \(arrayForRSA.count)")
+
+
